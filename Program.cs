c7268b99c65116a7725c1c789df59b52d39693f9
Justin Prim <justinprim140969@gmail.com>
2024-01-10 14:48:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9.Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] luckyNumbers = { 4, 5, 8, 9 };
+            Console.WriteLine(luckyNumbers[0]);
+
+            string[] friends = new string[9];
+            friends[0] = "Jeff";
+            friends[1] = "Jefferson";
+            friends[2] = "Jeffery";
+
+            Console.WriteLine(friends[0]);
+            Console.ReadLine();
+        }
+    }
+}
